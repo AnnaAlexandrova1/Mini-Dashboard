a6304719b679c677e6febb9c0fcc46f1888fa892
Anna Aleksandrova <any7487@yandex.ru>
2021-11-23 21:24:48 +0300

    border top does not work

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const item = document.querySelector('.item')
+const placeholder = document.querySelectorAll('placeholder')
+
 console.log(item);
 item.addEventListener('dragstart', dragstart);
 item.addEventListener('dragend', dragend);
+
+for (const placeholder of placeholders){
+    placeholder.addEventListener('dragover', dragover)
+    placeholder.addEventListener('dragenter', dragenter)
+    placeholder.addEventListener('dragleave', dragleave)
+    placeholder.addEventListener('drop', dragdrop)
+}
+
 
 function dragstart(event) {
 event.target.classList.add('hold')
@@ -9,6 +19,19 @@ setTimeout(() =>
 event.target.classList.add('hide'), 0)
 }
 function dragend(event) {
-
 event.target.classList.remove('hold', 'hide')
+}
+
+function dragover(event) {
+ console.log('drag over')  
+}
+function dragenter(event) {
+ event.target.classList.add('hovered')
+ console.log('drag enter')    
+}
+function dragleave(event) {
+ console.log('drag leave')    
+}
+function dragdrop(event) {
+ console.log('drag drop')   
 }
